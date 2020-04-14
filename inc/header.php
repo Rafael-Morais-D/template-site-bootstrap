7c203fc8959a53193a09a716951dc5a994dc664c
RafaelMorais <rafaelmorais.design@gmail.com>
2020-04-13 22:40:57 -0300

    Primeiro teste da aula 07 da DH

---
+++ inc/header.php
@@ -0,0 +1,31 @@
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="index.php">
+                <img src="https://slack.global.ssl.fastly.net/9427/img/icons/app-256.png" alt="Logo" width="30" height="30">
+                Umbrella Corp
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Início <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Produtos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contato</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" data-toggle="modal" data-target="#modalLogin">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#">Em breve, novidades...</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
---
+++ index.php
@@ -0,0 +1,125 @@
+<!-- Traz o head -->
+<?php require_once("./inc/head.php"); ?>
+<!-- Traz o header -->
+<?php require_once("./inc/header.php"); ?>
+
+<!-- Carrossel Bootstrap -->
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carrosselBootstrap" data-slide-to="0" class="active"></li>
+    <li data-target="#carrosselBootstrap" data-slide-to="1"></li>
+    <li data-target="#carrosselBootstrap" data-slide-to="2"></li>
+    <li data-target="#carrosselBootstrap" data-slide-to="4"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_1717605060d%20text%20%7B%20fill%3A%23555%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_1717605060d%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22285.9140625%22%20y%3D%22217.83125%22%3EPrimeiro%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="Primeiro slide">
+        <div class="carousel-caption d-none d-sm-block">
+            <h5>Título 1</h5>
+            <p>Descrição 1</p>
+        </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_1717605060f%20text%20%7B%20fill%3A%23444%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_1717605060f%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23666%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22247.3046875%22%20y%3D%22217.83125%22%3ESegundo%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="Segundo slide">
+        <div class="carousel-caption d-none d-sm-block">
+            <h5>Título 2</h5>
+            <p>Descrição 2</p>
+        </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_17176050610%20text%20%7B%20fill%3A%23333%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_17176050610%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23555%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22276.9921875%22%20y%3D%22217.83125%22%3ETerceiro%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="Terceiro slide">
+        <div class="carousel-caption d-none d-sm-block">
+            <h5>Título 3</h5>
+            <p>Descrição 3</p>
+        </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_17176050610%20text%20%7B%20fill%3A%23333%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_17176050610%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23555%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22276.9921875%22%20y%3D%22217.83125%22%3EQuarto%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="Quarto slide">
+        <div class="carousel-caption d-none d-sm-block">
+            <h5>Título 4</h5>
+            <p>Descrição 4</p>
+        </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carrosselBootstrap" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carrosselBootstrap" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+<main class="container">
+    <section id="catX" class="row">
+        <!-- Card Deck -->
+        <h2 class="col-12 text-center text-info mt-5 mb-3">Categoria X</h2>
+        <div class="card-deck">
+        <div class="card">
+            <img class="card-img-top" src=".../100px200/" alt="Card image cap">
+            <div class="card-body">
+            <h5 class="card-title">Produto X1</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <span class="badge badge-pill badge-info">PROMO</span>
+            </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top" src=".../100px200/" alt="Card image cap">
+            <div class="card-body">
+            <h5 class="card-title">Produto X2</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <span class="badge badge-pill badge-info">PROMO</span>
+            </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top" src=".../100px200/" alt="Card image cap">
+            <div class="card-body">
+            <h5 class="card-title">Produto X3</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <span class="badge badge-pill badge-info">PROMO</span>
+            </div>
+        </div>
+        </div>
+        </section>
+
+        <section id="catY" class="row">
+        <h2 h2 class="col-12 text-center text-info mt-5 mb-3">Categoria Y</h2>
+        <div class="card-deck">
+        <div class="card">
+            <img class="card-img-top" src=".../100px200/" alt="Card image cap">
+            <div class="card-body">
+            <h5 class="card-title">Produto Y1</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <span class="badge badge-pill badge-info">PROMO</span>
+            </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top" src=".../100px200/" alt="Card image cap">
+            <div class="card-body">
+            <h5 class="card-title">Produto Y2</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <span class="badge badge-pill badge-info">PROMO</span>
+            </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top" src=".../100px200/" alt="Card image cap">
+            <div class="card-body">
+            <h5 class="card-title">Produto Y3</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <span class="badge badge-pill badge-info">PROMO</span>
+            </div>
+        </div>
+        </div>
+        </section>
+</main>
+
+<!-- Traz o footer -->
+<?php require_once("./inc/footer.php"); ?>
+    
